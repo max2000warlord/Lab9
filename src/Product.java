3f8c1dac11c98817df3396f7f545c79b7109e9e5
max2000warlord <max2000warlord@users.noreply.github.com>
2017-05-26 04:43:25 +1000

    Updated

--- src/Product.java
+++ src/Product.java
@@ -3,7 +3,7 @@ import java.util.*;
 import javafx.beans.property.*;
 public class Product {
     private LinkedList<ProductObserver> observers = new LinkedList<ProductObserver>();
-    private final String name;
+    private final StringProperty name = new SimpleStringProperty();
     private IntegerProperty stock = new SimpleIntegerProperty();
     private final DoubleProperty price = new SimpleDoubleProperty();
     private IntegerProperty sold = new SimpleIntegerProperty();
@@ -17,19 +17,22 @@ public class Product {
         left.bind(sold.subtract(stock).multiply(-1));
     }
 
-    public final String getName() { return name; }
+    public final String getName() { return name.get(); }
+    public ReadOnlyStringProperty nameProperty() { return name; }
     public final int getStock() { return stock.get(); }
     public ReadOnlyIntegerProperty stockProperty() { return stock; }
     public final double getPrice() { return price.get();}
+    public ReadOnlyDoubleProperty priceProperty() { return price; }
     public final int getSold() { return sold.get(); }
     public ReadOnlyIntegerProperty soldProperty() { return sold; }
     public double getCash() { return cash.get(); }
+    
     public final int getLeft() { return left.get(); }
     public ReadOnlyIntegerProperty leftProperty() { return left; }
     
     public void sell(int n) {
         stock.set(getStock() - n);
-        cash.bind(price.multiply(n));
+        cash.add(price.multiply(n));
         sold.set(getSold() + n);
         for (ProductObserver observer : observers)
             observer.handleSell(getCash());
